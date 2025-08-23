5e0129d740828dc1f27946ba1c1bca6b54509900
Arian <ariannargesi@gmail.com>
2025-08-23 12:52:44 +0330

    Update ThemeHandler.ts
    made this file a bit more type safe

--- ThemeHandler.ts
+++ ThemeHandler.ts
@@ -1,35 +1,40 @@
+type Themes = "light" | "dark" | "system"
+
 export class ThemeHandler {
-    private static applyDark = () => {
-        document.documentElement.classList.toggle('dark', true)
-    }
-    private static applyLight = () => {
-        document.documentElement.classList.toggle('dark', false)
-    }
-    private static applySystemDefault = () => {
-        const isOS_dark = window.matchMedia("(prefers-color-scheme: dark)").matches
-        if (isOS_dark)
-            this.applyDark()
-        else this.applyLight()
-    }
-    static changeTheme = (value: 'light' | 'dark' | 'system') => {
-        if (value === 'light')
-            this.applyLight()
-        else if (value === 'dark')
-            this.applyDark()
-        else this.applySystemDefault()
-        localStorage.setItem('theme-mode', value)
-    }
-    static initiateTheme = () => {
-        const defaultTheme = localStorage.getItem('theme-mode')
-        if (defaultTheme !== 'light' && defaultTheme !== 'dark' && defaultTheme !== 'system')
-            return
-        if (defaultTheme === 'light')
-            this.changeTheme('light')
-        else if (defaultTheme === 'dark')
-            this.changeTheme('dark')
-        else this.changeTheme('system')
-    }
-    static getTheme = () => {
-        return localStorage.getItem('theme-mode') || 'system'
-    }
+  private static applyDark = () => {
+    document.documentElement.classList.toggle("dark", true)
+  }
+
+  private static applyLight = () => {
+    document.documentElement.classList.toggle("dark", false)
+  }
+
+  private static applySystemDefault = () => {
+    const isOS_dark = window.matchMedia("(prefers-color-scheme: dark)").matches
+    if (isOS_dark) this.applyDark()
+    else this.applyLight()
+  }
+
+  static changeTheme = (value: Themes) => {
+    if (value === "light") this.applyLight()
+    else if (value === "dark") this.applyDark()
+    else this.applySystemDefault()
+    localStorage.setItem("theme-mode", value)
+  }
+
+  static initiateTheme = () => {
+    const defaultTheme = localStorage.getItem("theme-mode")
+    if (
+      defaultTheme !== "light" &&
+      defaultTheme !== "dark" &&
+      defaultTheme !== "system"
+    )
+      return
+    if (defaultTheme === "light") this.changeTheme("light")
+    else if (defaultTheme === "dark") this.changeTheme("dark")
+    else this.changeTheme("system")
+  }
+
+  static getTheme = (): Themes =>
+    (localStorage.getItem("theme-mode") || "system") as Themes
 }
